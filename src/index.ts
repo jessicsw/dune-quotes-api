76a8d4679b9217a055ec47503174738ce5e7f344
jessicsw <jessicaselinewong@gmail.com>
2023-02-26 19:16:28 -0800

    Added error handling

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,81 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import v1RandomRouter from "./v1/routes/randomRoute";
 import v1QuotesRouter from "./v1/routes/quotesRoute";
 import v1BooksRouter from "./v1/routes/booksRoute";
-import * as dotenv from "dotenv";
 
-dotenv.config();
-const port = process.env.SERVER_PORT || 3000;
+class AppError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = Error.name;
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this);
+  }
+}
+
+const port = process.env.PORT || 3000;
 const app = express();
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+dotenv.config();
 
-// Middleware
+/* Middleware */
+app.use(apiLimiter);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const apiLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  max: 100, // Limit each IP to 100 requests per `window` (here, per hour)
-  standardHeaders: true,
-  legacyHeaders: false,
+app.use("/", (req, res) => {
+  throw new Error();
 });
-app.use(apiLimiter);
-
 app.use("/api/v1/random", v1RandomRouter);
 app.use("/api/v1/quotes", v1QuotesRouter);
 app.use("/api/v1/books", v1BooksRouter);
+
+/* Error handling middleware functions */
+const errorLogger = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(`error ${error.message}`);
+  next(error);
+};
+
+const errorResponder = (
+  error: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.header("Content-Type", "application/json");
+
+  const status = error.statusCode || 400;
+  res.status(status).send(error.message);
+};
+
+const invalidPathHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(400).send("Invalid path");
+};
+
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(invalidPathHandler);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
